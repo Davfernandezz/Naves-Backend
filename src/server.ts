f65b55627c2bbb203a78497af01b5b61929a28a2
David Fernadez <daffevafernandez@gmail.com>
2024-09-16 16:06:18 +0200

    feat: connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,21 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import { AppDataSource } from './database/db';
 
 const app = express();
-
+app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running. on port ${PORT}`);
-})
+AppDataSource.initialize()
+    .then(() => {
+        console.log('Database connected');
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    })
+    .catch(error => {
+        console.log(error)
+    })
