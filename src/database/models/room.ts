05e113b81795121b537e141779859ef5ecb1c5e8
David Fernadez <daffevafernandez@gmail.com>
2024-09-17 10:43:32 +0200

    feat: room completed model

--- src/database/models/room.ts
+++ src/database/models/room.ts
@@ -1,6 +1,16 @@
-import { Entity } from "typeorm"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class room {
+    @PrimaryGeneratedColumn()
+    id!: number;
 
+    @Column({ name: 'room_name' })
+    room_name!: string;
+
+    @Column({ name: 'capacity' })
+    capacity!: number;
+
+    @Column({ name: 'room_type' })
+    room_type!: string;
 }
