0fc65350093c064db756bc8e0f392c7547c3848c
David Fernadez <daffevafernandez@gmail.com>
2024-09-17 12:40:02 +0200

    feat: auth routes and auth controller created

---
+++ src/entities/auth/auth.controller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export const register = async (req: Request, res: Response) => {try {
+    
+} catch (error) {
+    
+}}
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
+import { router as authRoutes } from './entities/auth/auth.routes.js'
 
 const router = Router()
 
+router.use('/auth', authRoutes)
 
 export default router
