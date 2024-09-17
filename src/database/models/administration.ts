373ece02c4fef526af9e2d1b3087daa662087c0c
David Fernadez <daffevafernandez@gmail.com>
2024-09-17 10:45:50 +0200

    feat: administration completed model

--- src/database/models/administration.ts
+++ src/database/models/administration.ts
@@ -1,6 +1,22 @@
-import { Entity } from "typeorm"
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class administration {
+    @PrimaryGeneratedColumn()
+    id!: number
 
+    @Column({ name: 'report_date' })
+    report_date!: Date
+
+    @Column({ name: 'total_accesses' })
+    total_accesses!: number
+
+    @Column({ name: 'total_absences' })
+    total_absences!: number
+
+    @Column({ name: 'frequent_persons' })
+    frequent_users!: string
+
+    @Column({ name: 'infrequent_persons' })
+    infrequent_users!: string
 }
