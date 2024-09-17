ac9226f1d0d503817416ac38fec0a8fa11cbb020
David Fernadez <daffevafernandez@gmail.com>
2024-09-17 10:05:15 +0200

    feat: creation of migrations

---
+++ src/database/migrations/1726560167856-person.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Person1726560167856 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/database/migrations/1726560205798-room.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Room1726560205798 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
