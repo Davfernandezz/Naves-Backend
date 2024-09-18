0a15347901b86eafef9a7075f7f2f1bd9762dd56
David Fernadez <daffevafernandez@gmail.com>
2024-09-18 10:57:19 +0200

    feat: creation of authentication middleware and types

---
+++ src/types/index.d.ts
@@ -0,0 +1,19 @@
+import { Request } from 'express';
+
+export interface TokenDecoded {
+    id: number,
+    role: 'user' | 'admin';
+    email: string
+}
+
+declare global {
+    namespace Express {
+        export interface Request {
+            tokenData?: {
+                id: number;
+                role: 'user' | 'admin';
+                email: string;
+            };
+        }
+    }
+}
