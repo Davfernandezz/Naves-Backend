045f627910021733d468d79da3f34df38fa3fb0e
David Fernadez <daffevafernandez@gmail.com>
2024-09-22 16:27:22 +0200

    feat: getRoomAccessHistories created in accessHitory controller

--- src/entities/accessHistory/accessHistory.routes.ts
+++ src/entities/accessHistory/accessHistory.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { auth } from "../../middlewares/auth";
-import { getAccessHistories } from "./accessHitory.controller";
+import { getAccessHistories, getRoomAccessHistories } from "./accessHitory.controller";
 
 const router = Router()
 
 router.get('/',auth, getAccessHistories)
+router.get('/room/:room_id',auth, getRoomAccessHistories)
 
 export { router }
