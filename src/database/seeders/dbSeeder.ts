69ad4f088575e43033f938628b173cd170d9e5b1
David Fernadez <daffevafernandez@gmail.com>
2024-09-22 17:01:08 +0200

    feat: roomSeeder created

--- src/database/seeders/dbSeeder.ts
+++ src/database/seeders/dbSeeder.ts
@@ -1,7 +1,8 @@
 import { personsSeeder } from "./personsSeeder";
+import { roomsSeeder } from "./roomsSeeder";
 
 (async () => {
     console.log("Starting seeders...");
     await personsSeeder();
-
+    await roomsSeeder();
 })();
